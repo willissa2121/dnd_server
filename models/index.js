677074202e9c1254f6fdfd755ae2e1d5f410ef40
willissa2121 <you@example.com>
2020-04-07 15:22:31 -0700

    working db

--- models/index.js
+++ models/index.js
@@ -5,31 +5,38 @@ var basename = path.basename(__filename);
 var env = process.env.NODE_ENV || "development";
 var db = {};
 
-if (!global.hasOwnProperty("models")) {
-  var sequelize;
-  var Sequelize = require("sequelize");
+if (!global.hasOwnProperty("db")) {
+  var Sequelize = require("sequelize"),
+    sequelize = null;
 
-  if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
-    // the application is executed on Heroku ... use the postgres         database
-    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+  if (process.env.HEROKU_POSTGRESQL_BLACK_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BLACK_URL, {
       dialect: "postgres",
       protocol: "postgres",
-      port: 5432,
-      host: "<heroku host>",
+      port: match[4],
+      host: match[3],
       logging: true, //false
     });
   } else {
     // the application is executed on the local machine ... use mysql
-    const sequelize = new Sequelize("database", "username", "password", {
+    sequelize = new Sequelize("leaguestats_db", "root", "Passwordsucks!1", {
       host: "localhost",
       dialect: "mysql",
     });
   }
-  global.models = {
+
+  global.db = {
     Sequelize: Sequelize,
     sequelize: sequelize,
     diceRoll: sequelize.import(__dirname + "/diceRoll"),
     // add your other models here
   };
+
+  /*
+    Associations can be defined here. E.g. like this:
+    global.db.User.hasMany(global.db.SomethingElse)
+  */
 }
-module.exports = global.models;
+
+module.exports = global.db;
