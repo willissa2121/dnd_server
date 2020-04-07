1f21ba546389221fdef5a0b5e94d01827f63c43d
willissa2121 <you@example.com>
2020-04-07 14:59:56 -0700

    working

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const db = require("./models");
 
 const routes = require("./routes/routes");
+app.use(allowCrossDomain)
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
