2f1a358d2e4c27d3f700b7f64179f61da5d3a354
willissa2121 <you@example.com>
2020-04-07 15:18:03 -0700

    working db

--- models/index.js
+++ models/index.js
@@ -5,41 +5,30 @@ var basename = path.basename(__filename);
 var env = process.env.NODE_ENV || "development";
 var db = {};
 
-if (process.env.HEROKU_POSTGRESQL_BLACK_URL) {
-  // the application is executed on Heroku ... use the postgres database
-  sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BLACK_URL, {
-    dialect: "postgres",
-    protocol: "postgres",
-    port: match[4],
-    host: match[3],
-    logging: true, //false
-  });
-} else {
-  // the application is executed on the local machine ... use mysql
-  var sequelize = new Sequelize("leaguestats_db", "root", "Passwordsucks!1", {
-    host: "localhost",
-    dialect: "mysql",
-  });
-}
-
-fs.readdirSync(__dirname)
-  .filter((file) => {
-    return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
-  })
-  .forEach((file) => {
-    var model = sequelize["import"](path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+if (!global.hasOwnProperty("models")) {
+  var Sequelize = require("sequelize"),
+    sequelize = null;
+  if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+    // the application is executed on Heroku ... use the postgres         database
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+      dialect: "postgres",
+      protocol: "postgres",
+      port: 5432,
+      host: "<heroku host>",
+      logging: true, //false
+    });
+  } else {
+    // the application is executed on the local machine ... use mysql
+    const sequelize = new Sequelize("database", "username", "password", {
+      host: "localhost",
+      dialect: "mysql",
+    });
   }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+  global.models = {
+    Sequelize: Sequelize,
+    sequelize: sequelize,
+    diceRoll: sequelize.import(__dirname + "/diceRoll"),
+    // add your other models here
+  };
+}
+module.exports = global.models;
