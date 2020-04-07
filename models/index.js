0aa43e3932b1ff6d3ef81476ea632895ad5f32cb
willissa2121 <you@example.com>
2020-04-07 14:34:38 -0700

    sequelize app

--- models/index.js
+++ models/index.js
@@ -1,25 +1,36 @@
-var fs = require('fs');
-var path = require('path');
-var Sequelize = require('sequelize');
+var fs = require("fs");
+var path = require("path");
+var Sequelize = require("sequelize");
 var basename = path.basename(__filename);
-var env = process.env.NODE_ENV || 'development';
+var env = process.env.NODE_ENV || "development";
 var db = {};
 
+if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  var sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    port: match[4],
+    host: match[3],
+    logging: true, //false
+  });
+} else {
+  // the application is executed on the local machine ... use mysql
+  var sequelize = new Sequelize("leaguestats_db", "root", "Passwordsucks!1", { host: "localhost", dialect: 'mysql' });
+}
 
-var sequelize = new Sequelize('leaguestats_db', 'root', 'Passwordsucks!1', { host: "localhost", dialect: 'mysql' });
-
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
   })
-  .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
+  .forEach((file) => {
+    var model = sequelize["import"](path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
