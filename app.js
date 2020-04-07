5748808ca51fc14dd2fdf109618d3869eaccf0ef
willissa2121 <you@example.com>
2020-04-07 14:53:05 -0700

    fixing

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const bodyParser = require("body-parser");
 const db = require("./models");
 
 const routes = require("./routes/routes");
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 
 app.use(cors());
 const port = process.env.PORT || 3001;
